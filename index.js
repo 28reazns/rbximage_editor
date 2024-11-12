277efb31fb9cc3095c8dd1634a1c08acfb77bfee
evunei <80120337+evunei@users.noreply.github.com>
2024-11-12 16:08:47 -0500

    bug testing

--- index.js
+++ index.js
@@ -4,38 +4,47 @@ const https = require('https');
 const Jimp = require('jimp').Jimp;
 
 
-const ids = (process.argv[2].split(","))
-
+const ids = (process.argv[2] && process.argv[2].split(","))
+console.log(ids)
 
 async function getData(id) {
   const url = "https://assetdelivery.roproxy.com/v1/asset?id=" + id;
   https.get(url, (response) => {
-    const file = fs.createWriteStream("images/" + id + ".png");
-    response.pipe(file);
+    if(response.statusCode!=200){
+      fs.unlink("images/" + id + ".png"); // Delete the file if an error occurs
+      console.error(err);
+      return false
+    }else{
+      const file = fs.createWriteStream("images/" + id + ".png");
+      response.pipe(file);
 
-    file.on('finish', () => {
-      file.close();
+      file.on('finish', () => {
+        file.close();
+      });
       return true;
-    });
+    }
   }).on('error', (err) => {
-    fs.unlink(filePath); // Delete the file if an error occurs
+    fs.unlink("images/" + id + ".png"); // Delete the file if an error occurs
     console.error(err);
     return false;
   });
 }
 
 async function loadIds() {
-  let downloaded = 0;
+  let downloadedfiles = 0;
   if (ids){
     ids.forEach(element => {
       getData(element).then((bool)=>{
+        console.log(bool)
         if(bool){
-          downloaded+=1
+          downloadedfiles+=1
         }
       })
     });
+    return downloadedfiles
   }
-  return downloaded
+  return -1
+  
 }
 
 loadIds().then((downloaded)=>{
